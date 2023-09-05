130f4b67e6bac6b426fcabecb13adafe7c91bc59
Dev-KUN-F <k3614900@gmail.com>
2023-09-05 20:23:23 +0900

    레이아웃 구조확립 작성은아직

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
+import Layout from "../src/components/commons/layout";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   const client = new ApolloClient({
@@ -11,7 +11,9 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
 
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ApolloProvider>
   );
 }
