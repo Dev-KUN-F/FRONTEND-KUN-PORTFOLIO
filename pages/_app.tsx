ca55a1e76a82783e72f0759b2240ece9d9a82733
Dev-KUN-F <k3614900@gmail.com>
2023-09-05 22:04:39 +0900

    글로벌스타일로 글로벌폰트와 css 적용 완료

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,18 @@ import "../styles/globals.css";
 
 import Layout from "../src/components/commons/layout";
 import ApolloSetting from "../src/components/commons/apollo";
+import { Global } from "@emotion/react";
+import { globalStyles } from "../src/commons/styles/globalStyles";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ApolloSetting>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <>
+        <Global styles={globalStyles} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
     </ApolloSetting>
   );
 }
