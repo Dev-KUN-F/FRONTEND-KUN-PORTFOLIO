a4e0f92c645c382ec7e02c7cf5df14c214bb5678
Dev-KUN-F <k3614900@gmail.com>
2023-09-05 21:59:03 +0900

    네비게이션바 작성 동작확인 레이아웃구성 완료

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -1,6 +1,14 @@
 import LayoutBanner from "./banner/LayoutBanner.container";
 import LayoutHeader from "./header/LayoutHeader.container";
 import LayoutNavigation from "./navigation/LayoutNavigation.container";
+import styled from "@emotion/styled";
+
+const Body = styled.div`
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 interface ILayoutProps {
   children: JSX.Element;
@@ -12,7 +20,7 @@ export default function Layout(props: ILayoutProps): JSX.Element {
       <LayoutHeader />
       <LayoutBanner />
       <LayoutNavigation />
-      <div>{props.children}</div>
+      <Body>{props.children}</Body>
     </>
   );
 }
--- src/components/commons/layout/navigation/LayoutNavigation.container.tsx
+++ src/components/commons/layout/navigation/LayoutNavigation.container.tsx
@@ -1,3 +1,13 @@
+import { useRouter } from "next/router";
+import LayoutNavigationUI from "./LayoutNavigation.presenter";
+import type { MouseEvent } from "react";
+
 export default function LayoutNavigation(): JSX.Element {
-  return <div>navi</div>;
+  const router = useRouter();
+
+  const onClickMenu = (event: MouseEvent<HTMLDivElement>): void => {
+    void router.push(event.currentTarget.id);
+  };
+
+  return <LayoutNavigationUI onClickMenu={onClickMenu} />;
 }
